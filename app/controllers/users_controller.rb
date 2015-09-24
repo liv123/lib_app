43fa9fe54182dd62da0f483bcc95b142d7a0b48c
liv123 <olivia.e@gmail.com>
2015-09-23 20:55:33 -0700

    Added new user create route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,15 @@ class UsersController < ApplicationController
 		@users = User.all
 		render :index
 	end	
+
+	def new
+		@user = User.new
+		render :new
+	end
+	
+	def create
+		user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
+		@user = User.create(user_params)
+		redirect_to '/users'
+	end	
 end
